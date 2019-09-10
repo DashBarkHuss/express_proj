3b074796d312378752870beb03edd1e6e8b9ed40
Dashiell BarkHuss <dashiellbarkhuss@Dashiells-MacBook-Pro.local>
2019-09-10 14:05:14 -0500

    started post

--- index.js
+++ index.js
@@ -13,6 +13,15 @@ app.get('/', (req, res)=>{
 
 })
 
+// app.post('/', (req, res)=>{
+//     const q = `insert into realitychecks (time, location, user_id) values('${req.body.time}','')`;
+
+//     database.connection.query(q, (err, results)=>{
+//         console.log(err, res);
+//         res.send(results.map(x=>x.username))
+//     })
+
+// })
 
 app.listen(3306, ()=>{console.log("listening")})
 
