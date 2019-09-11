b96219fa2b2783aa6961310f5e9fd9715aa7d557
Dashiell BarkHuss <dashiellbarkhuss@Dashiells-MacBook-Pro.local>
2019-09-11 12:32:49 -0500

    date type not lining up

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const database = require('./database');
 const fetch = require('node-fetch');
 const app = express();
 
-database.create();
 
+database.create();
+app.use(express.json());
+
+
+app.use(express.static("./"))
 app.get('/', (req, res)=>{
     database.connection.query('select * from user', (err, results)=>{
         console.log(err, res);
@@ -13,17 +17,25 @@ app.get('/', (req, res)=>{
 
 })
 
-// app.post('/', (req, res)=>{
-//     const q = `insert into realitychecks (time, location, user_id) values('${req.body.time}','')`;
 
-//     database.connection.query(q, (err, results)=>{
-//         console.log(err, res);
-//         res.send(results.map(x=>x.username))
-//     })
+app.post('/rc', (req, res)=>{
+    console.log(req.body);
+    const q = `insert into reality_checks (time, location, user_id) values('${req.body.rcInfo.timestamp}','${req.body.rcInfo.geolocation}', '${req.body.user_id}')`;
 
-// })
+    database.connection.query(q, (err, results)=>{
+        console.log(err, results);
+        res.send(results)
+    })
+
+})
 
 app.listen(3306, ()=>{console.log("listening")})
 
-fetch('http://127.0.0.1:3306/').then(x=>{console.log('done')})
-
+// fetch('http://127.0.0.1:3306/test',         {
+//     method: "POST",
+//     body: JSON.stringify({user_id: 1}),
+//     body: JSON.stringify({"rcInfo":{"timestamp":1568221782150,"user_id":1}}),
+//     headers: {
+//       "Content-Type": "application/json"
+//     }
+//   }).then(x=>{console.log('done')})
