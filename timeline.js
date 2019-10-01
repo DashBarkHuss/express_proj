456a073672824beb2e9acd1574eaaed19b05f3bb
Dashiell BarkHuss <dashiellbarkhuss@gmail.com>
2019-10-01 12:00:37 -0500

    tooltips timeline fixes

--- timeline.js
+++ timeline.js
@@ -1,56 +1,65 @@
 const renderTimeLine=(times)=>{
     
     const timeline = document.getElementById("timeline");
+    const timelineColor = 'lightblue';
+    const textColor = 'lightblue';
     timeline.innerHTML = "";
     console.log("tl");
-    const timespan = times[times.length-1]-times[0];
-
+    
     const line = document.createElementNS('http://www.w3.org/2000/svg','line')
-    line.setAttributeNS(null, 'x1',   0);
-    line.setAttributeNS(null, 'x2', 500);
+    const svgWidth = 500 
+    const radius = 4;
+    const margin = radius +20;
+    const lineWidth = svgWidth-margin*2;
+    const firstRC = (()=>{
+        return times.reduce((minimum, time) => time < minimum ? time : minimum, times[0]);
+    })()
+    const timespan = times[times.length-1]-firstRC;
+    line.setAttributeNS(null, 'x1',   margin);
+    line.setAttributeNS(null, 'x2', lineWidth+margin);
     line.setAttributeNS(null, 'y1',   50);
     line.setAttributeNS(null, 'y2',   50);
-    line.setAttributeNS(null, 'stroke', 'pink');
+    line.setAttributeNS(null, 'stroke', timelineColor);
     line.setAttributeNS(null, 'stroke-width', 1.5);
     document.getElementById("timeline").appendChild(line);
     for (i=0; i<times.length;i++){  
-        const radius = 5;
+        const percentage = (times[i]-firstRC)/timespan;
         const text = document.createElementNS('http://www.w3.org/2000/svg','text');      
-        text.setAttributeNS(null, 'x', (times[i]-times[0])/timespan * (500-2*radius));
-        text.setAttributeNS(null, 'y', 40);
-        text.setAttributeNS(null, 'display', 'none');
+        text.setAttributeNS(null, 'x', (percentage * lineWidth + margin));
+        text.setAttributeNS(null, 'y', 50-radius-4);
         text.setAttributeNS(null, 'id', 't'+i);
-        text.setAttribute('fill', '#000');
+        text.setAttribute('fill', textColor);
         text.textContent = epochToTime(times[i]);
-
+        
         const circle = document.createElementNS('http://www.w3.org/2000/svg','circle');
-        circle.setAttributeNS(null, 'cx', (times[i]-times[0])/timespan * (500-2*radius)+radius)
+        circle.setAttributeNS(null, 'cx', (percentage * lineWidth + margin));
         circle.setAttributeNS(null, 'cy', 50)
         circle.setAttributeNS(null, 'r', radius)
         circle.setAttributeNS(null, 'id', "c"+i)
-        circle.setAttributeNS(null, 'fill', 'pink');
+        circle.setAttributeNS(null, 'fill', timelineColor);
         
         timeline.appendChild(circle);
         timeline.appendChild(text);
+        text.setAttribute('transform',`translate(-${text.getBBox().width/2},0)`);
+        text.setAttributeNS(null, 'display', 'none');
     }
     // tooltip
     timeline.addEventListener('mouseenter', (e)=>{
-        console.log(e.target);
         if(e.target.tagName=="circle"){ 
-            console.log(e.target.id);
+            e.target.setAttribute('stroke', timelineColor)
+            e.target.setAttribute('stroke-width', `${radius*.6}`)
             id = e.target.id.substring(1); 
             elem = document.getElementById('t'+id); 
             elem.setAttribute('display','block'); 
-            console.log(elem)}}
+        }
+    }
     , true)
     timeline.addEventListener('mouseleave', (e)=>{
-        console.log(e.target);
-            if(e.target.tagName=="circle"){
-                console.log(e.target.id);
+        if(e.target.tagName=="circle"){
+            e.target.setAttribute('stroke-width', `none`)
                 id = e.target.id.substring(1); 
                 elem = document.getElementById('t'+id);
                 elem.setAttribute('display','none'); 
-                console.log(elem);
             }
         }
     , true)
